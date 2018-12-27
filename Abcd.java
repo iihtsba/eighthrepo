b3695737771cace6fe8641f9e5dc8f6a06be0cde
iihtsba <43966421+iihtsba@users.noreply.github.com>
2018-12-27 13:29:12 +0530

    Update Abcd.java

--- Abcd.java
+++ Abcd.java
@@ -2,7 +2,7 @@ public class Abcd{
 public static void main(String args[])
 {
 	System.out.println("main method in Abcd");
-	System.out.println("This is another  message");
+	System.out.println("This is another  message. Testing purposeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
 	
 	new Mnop().met();
 }
